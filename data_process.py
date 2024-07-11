dbcb4339c70be40f3393b7218aa07df948ed2718
hanjiel <hanjiel@sfu.ca>
2024-07-10 17:07:44 -0700

    reproduce process script and data

---
+++ data_process.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import sys
+
+#python data_lprocess.py Walk1.csv walk
+
+file_name = sys.argv[1]
+label_name = sys.argv[2]
+
+data = pd.read_csv(f"Data/{file_name}")
+start_time = pd.to_datetime(data['time'].iloc[0])
+
+data['time'] = pd.to_datetime(data['time'])
+data['time'] = ((data['time'] - start_time).dt.total_seconds() // 0.01 * 0.01)
+
+data = data.groupby('time').agg({'ax':'mean', 'ay':'mean','az':'mean','speed':'mean'}).reset_index()
+
+selected_columns = ['time','ax','ay','az','speed']
+new_data = data[selected_columns]
+new_data['label'] = label_name
+
+#keep rounding
+new_data[['time','ax','ay','az']] = new_data[['time','ax','ay','az']].round(3)
+
+
+
+#print(new_data)
+new_data.to_csv(f"Data/{file_name}", index = False)
+
