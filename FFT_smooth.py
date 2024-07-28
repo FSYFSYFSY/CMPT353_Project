c7d9778ce94acad1d514eac1511592e0cf69e490
Fsy <12874184+FSYFSYFSY@user.noreply.gitee.com>
2024-07-27 22:15:23 -0700

    fix issues of FFT_smooth

--- FFT_smooth.py
+++ FFT_smooth.py
@@ -2,12 +2,23 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.fft import fft, ifft
+import sys
 
 def fft_denoise(signal, threshold=0.1):
-    signal_fft = fft(signal)
+    signal_fft = fft(signal, axis=0)
     freqs = np.fft.fftfreq(len(signal))
     signal_fft[np.abs(freqs) > threshold] = 0
     cleaned_signal = ifft(signal_fft)
     return cleaned_signal.real
 
-#data_denoised = data.apply(fft_denoise, threshold=0.05)
+file_name = sys.argv[1]
+df = pd.read_csv(f"Data/{file_name}")
+
+print(df)
+
+data_denoised = fft_denoise(df['ax'].values, threshold=0.2)
+
+df['ax_denoised'] = data_denoised
+
+print(df)
+
